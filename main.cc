57c46dedb56c7e2bfeba3def935dfca8b58d4f9a
Willem Melching <willem.melching@gmail.com>
2020-04-04 15:04:03 -0700

    Add two vectors

--- main.cc
+++ main.cc
@@ -1,8 +1,11 @@
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+
 #include <iostream>
 #include <cassert>
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 #include <CL/cl.h>
 
@@ -104,11 +107,72 @@ int main( int argc, char *argv[] ) {
   cl_context ctx = clCreateContext(NULL, 1, &d, NULL, NULL, &err);
   cl_ok(err);
 
+  cl_command_queue q = clCreateCommandQueue(ctx, d, 0, &err);
+  cl_ok(err);
 
   auto source = read_file("../test.cl");
   std::cout << source << std::endl;
 
   auto program = build(ctx, d, source);
 
+  // Build input buffers
+  const size_t sz = 128;
+  const size_t buf_sz = sizeof(int) * sz;
+  int * A = new int[sz];
+  int * B = new int[sz];
+  int * out = new int[sz];
+
+  for (size_t i = 0; i < sz; i++){
+    A[i] = i;
+    B[i] = 100;
+  }
+  auto A_cl = clCreateBuffer(ctx, CL_MEM_READ_ONLY, buf_sz, NULL, &err);
+  cl_ok(err);
+  auto B_cl = clCreateBuffer(ctx, CL_MEM_READ_ONLY, buf_sz, NULL, &err);
+  cl_ok(err);
+  auto out_cl = clCreateBuffer(ctx, CL_MEM_WRITE_ONLY, buf_sz, NULL, &err);
+  cl_ok(err);
+
+  // Create kernel
+  cl_kernel kernel = clCreateKernel(program, "vector_add", &err);
+  cl_ok(err);
+
+  // Set kernel arguments
+  cl(clSetKernelArg(kernel, 0, sizeof(A_cl), &A_cl));
+  cl(clSetKernelArg(kernel, 1, sizeof(B_cl), &B_cl));
+  cl(clSetKernelArg(kernel, 2, sizeof(out_cl), &out_cl));
+
+  // Copy A and B into device memory
+  cl(clEnqueueWriteBuffer(q, A_cl, CL_TRUE, 0, buf_sz, A, 0, NULL, NULL));
+  cl(clEnqueueWriteBuffer(q, B_cl, CL_TRUE, 0, buf_sz, B, 0, NULL, NULL));
+
+  // Queue kernel
+  size_t N = 8;
+  cl(clEnqueueNDRangeKernel(q, kernel, 1, NULL, &sz, &N, 0, NULL, NULL));
+
+  // Read output
+  cl(clEnqueueReadBuffer(q, out_cl, CL_TRUE, 0, buf_sz, out, 0, NULL, NULL));
+
+
+  // Wait for opencl to finish
+  cl(clFlush(q));
+  cl(clFinish(q));
+
+  for (size_t i = sz - 10; i < sz; i++){
+    std::cout << "out[" << i << "]: " << out[i] << std::endl;
+  }
+
+  // Cleanup
+  delete[] A;
+  delete[] B;
+  delete[] out;
+
+
+  cl(clReleaseKernel(kernel));
+  cl(clReleaseProgram(program));
+  cl(clReleaseMemObject(A_cl));
+  cl(clReleaseMemObject(B_cl));
+  cl(clReleaseMemObject(out_cl));
+  cl(clReleaseCommandQueue(q));
   cl(clReleaseContext(ctx));
 }
