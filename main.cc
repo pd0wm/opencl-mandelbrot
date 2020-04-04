ac95c828fa1de7000ee735850541b2fd43022582
Willem Melching <willem.melching@gmail.com>
2020-04-04 13:44:18 -0700

    create context

--- main.cc
+++ main.cc
@@ -8,23 +8,18 @@
 
 #define UNUSED(x) (void)(x)
 
-std::string get_device_info(cl_device_id device, cl_device_info param_name){
+std::string get_device_info_string(cl_device_id device, cl_device_info param_name){
   size_t value_sz;
   cl(clGetDeviceInfo(device, param_name, 0, NULL, &value_sz));
   char * value = new char[value_sz];
   cl(clGetDeviceInfo(device, param_name, value_sz, value, NULL));
 
   std::string r = std::string(value);
-
   delete[] value;
-
   return r;
 }
 
 cl_device_id get_first_device(void){
-  cl_device_id result = NULL;
-
-
   cl_uint num_platforms;
   cl(clGetPlatformIDs(0, NULL, &num_platforms));
 
@@ -43,14 +38,14 @@ cl_device_id get_first_device(void){
     }
 
     // Get first device
+    cl_device_id result = NULL;
     cl(clGetDeviceIDs(platforms[platform_idx], CL_DEVICE_TYPE_ALL, 1, &result, NULL));
     delete[] platforms;
     return result;
   }
 
   delete[] platforms;
-
-  return result;
+  return NULL;
 }
 
 
@@ -58,14 +53,21 @@ int main( int argc, char *argv[] ) {
   UNUSED(argc);
   UNUSED(argv);
 
+  cl_int err;
+
   cl_device_id d = get_first_device();
   assert(d);
 
-  std::cout << "Device name: " << get_device_info(d, CL_DEVICE_NAME) << std::endl;
-  std::cout << "OpenCL C version: " << get_device_info(d, CL_DEVICE_OPENCL_C_VERSION) << std::endl;
+  std::cout << "Device name: " << get_device_info_string(d, CL_DEVICE_NAME) << std::endl;
+  std::cout << "OpenCL C version: " << get_device_info_string(d, CL_DEVICE_OPENCL_C_VERSION) << std::endl;
 
   cl_uint compute_units;
   cl(clGetDeviceInfo(d, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(compute_units), &compute_units, NULL));
   std::cout << "Compute units: " << compute_units << std::endl;
 
+  cl_context context = clCreateContext(NULL, 1, &d, NULL, NULL, &err);
+  cl_ok(err);
+
+
+  cl(clReleaseContext(context));
 }
